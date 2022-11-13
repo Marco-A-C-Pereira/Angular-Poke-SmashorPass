9e77e2af983ef0e4c3043d3df8e017e8f0a239d5
unknown <finfokka@gmail.com>
2022-11-13 18:37:54 -0300

    Primeiras alterações do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadderComponent } from './components/shared/headder/headder.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TestCardComponent } from './components/test-card/test-card.component';
+import { PokeTypeComponent } from './components/shared/poke-type/poke-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadderComponent,
+    FooterComponent,
+    HomeComponent,
+    TestCardComponent,
+    PokeTypeComponent,
   ],
   imports: [
     BrowserModule
